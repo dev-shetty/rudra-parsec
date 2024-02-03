fc918a943a32a1d5dd42507af12b92c0aa45df78
Deveesh Shetty <deveeshshetty@gmail.com>
2024-02-03 08:03:58 +0530

    adds route to get user

--- web/client/app/register/page.tsx
+++ web/client/app/register/page.tsx
@@ -1,13 +1,9 @@
 import AuthForm from "@/components/auth-form"
 
-export default function Login({
-  searchParams,
-}: {
-  searchParams: { message: string }
-}) {
+export default function Login() {
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      <AuthForm mode="signin" />
+      <AuthForm mode="signup" />
     </div>
   )
 }
