6e9c467a0c46637067dd50be59bd3f1b5194c036
srajan <kumarsrajan02@gmail.com>
2024-02-03 07:10:22 +0530

    create-pot done

--- web/client/app/(pot)/create-pot/page.tsx
+++ web/client/app/(pot)/create-pot/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 const page = () => {
+  const [loading, isLoading] = useState(false);
   return (
     <div className="container relative min-h-[100dvh] flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-muted text-white lg:flex">
@@ -19,12 +31,28 @@ const page = () => {
       <div className="lg:p-8">
         <div className="mx-auto flex flex-col justify-center space-y-6 w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">
-              Login to continue
-            </h1>
+            <h1 className="text-2xl font-semibold tracking-tight">POTs</h1>
           </div>
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>Join an existing POT</AccordionTrigger>
+              <AccordionContent>
+                <div className="flex gap-3">
+                  <Input
+                    placeholder="Enter the code"
+                    className="w-full focus-visible:ring-0 focus-visible:ring-transparent focus-visible:border-white"
+                  />
+                  <Button>
+                    <ArrowRight className="w-5 h-5" />
+                  </Button>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+
+          <Button>Create a new POT</Button>
           {/* <UserAuthForm /> */}
-          <p className="px-8 text-center text-sm text-muted-foreground">
+          {/* <p className="px-8 text-center text-sm text-muted-foreground">
             Do not have an account?{" "}
             <Link
               href="/register"
@@ -32,7 +60,7 @@ const page = () => {
             >
               Register
             </Link>
-          </p>
+          </p> */}
         </div>
       </div>
     </div>
--- web/client/components/Navbar.tsx
+++ web/client/components/Navbar.tsx
@@ -1,12 +1,16 @@
 import AuthButton from "@/components/AuthButton";
 import Link from "next/link";
-
+import { Button } from "./ui/button";
 const Navbar = () => {
   return (
-    <div className="fixed top-5 w-full flex justify-center items-center">
-      <div className="bg-white/20 flex items-center justify-between backdrop-blur-md rounded-full px-5 max-w-7xl w-full mx-5">
-        <AuthButton />
-        <Link href="pot">Pot</Link>
+    <div className="fixed bottom-20 w-full flex justify-center items-center">
+      <div className="bg-white/20 flex py-2 items-center justify-center backdrop-blur-md rounded-full px-5 max-w-7xl w-full mx-5">
+        {/* <AuthButton /> */}
+        <Link href="/create-pot">
+          <Button variant={"link"} className="text-primary/50 hover:text-white">
+            POTs
+          </Button>
+        </Link>
       </div>
     </div>
   );
