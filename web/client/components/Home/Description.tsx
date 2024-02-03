1f2103abe74b4178dae38687ee661d59a6e3b4c4
srajan <kumarsrajan02@gmail.com>
2024-02-03 16:20:33 +0530

    some more things in landing page

--- web/client/components/Home/Description.tsx
+++ web/client/components/Home/Description.tsx
@@ -1,15 +1,36 @@
+"use client";
 import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
-const Description = () => {
+const Cards = () => {
   return (
-    <div className="flex justify-center items-center">
-      <div className="max-w-3xl text-2xl text-center">
+    <div className="flex flex-col mb-10 md:mb-10 w-full justify-center items-center px-8">
+      <motion.div
+        className="rounded-md"
+        initial={{ y: 100, opacity: 0 }} // Image starts from 100px below and fully transparent
+        animate={{ y: 0, opacity: 1 }} // Image ends at its original position and fully opaque
+        transition={{ type: "spring", stiffness: 50, damping: 20 }} // transition specifications
+      >
+        <AnimatePresence mode="wait">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.25 }}
+          >
+            <div className="max-w-7xl mb-20">
+              <img src="/cover.png" alt="" />
+            </div>
+          </motion.div>
+        </AnimatePresence>
+      </motion.div>
+      <p className="max-w-4xl md:pt-10 leading-[2.5rem] text-2xl sm:text-3xl text-center">
         Empowering financial wisdom through education, engagement, and
         collaborative savings - where informed decisions shape a secure and
         transparent financial future.
-      </div>
+      </p>
     </div>
   );
 };
 
-export default Description;
+export default Cards;
